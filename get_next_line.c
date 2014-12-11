0626f468a2912430fbbe2762ef9027379cb853f7
rmangili <rmangili@voila.fr>
2014-12-11 11:51:08 +0100

    Sup

---
+++ get_next_line.c
@@ -0,0 +1,77 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmangili <rmangili@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/11/22 14:39:29 by rmangili          #+#    #+#             */
+/*   Updated: 2014/12/09 15:22:40 by rmangili         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "get_next_line.h"
+#include <unistd.h>
+#include "libft.h"
+#include <fcntl.h>
+
+static int		get_read_size(char **str, int fd)
+{
+	int		i;
+	char	buf[BUF_SIZE];
+	char	*s;
+
+	i = read (fd, buf, BUF_SIZE);
+	if (i == -1)
+		return (-1);
+	buf[i] = 0;
+	s = *str;
+	*str = ft_strjoin(*str, buf);
+	if (*s != 0)
+		free(s);
+	return (i);
+}
+
+static int		get_fill_line(char **str, char **line, char *s)
+{
+	char	*zebre;
+	int		i;
+
+	i = 0;
+	if (*s == '\n')
+		i = 1;
+	*s = 0;
+	*line = ft_strjoin("", *str);
+	zebre = *str;
+	*str = ft_strjoin(s + 1, "");
+	free(zebre);
+	return (i);
+}
+
+int				get_next_line(int const fd, char **line)
+{
+	char		*s;
+	static char	*str;
+	int			i;
+
+	if (str == 0)
+		str = "";
+	if (!line || BUF_SIZE < 1)
+		return (-1);
+	i = BUF_SIZE;
+	while (1)
+	{
+		s = str;
+		while (*s || i < BUF_SIZE)
+		{
+			if (*s == '\n' || *s == 0 || *s == -1)
+				return (get_fill_line(&str, line, s));
+			s++;
+		}
+		i = get_read_size(&str, fd);
+		if (i == -1)
+			return (-1);
+	}
+}
---
+++ get_next_line.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmangili <rmangili@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/11/22 15:17:39 by rmangili          #+#    #+#             */
+/*   Updated: 2014/12/04 14:59:37 by rmangili         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUF_SIZE 65
+
+int				get_next_line(int const fd, char **line);
+
+#endif
